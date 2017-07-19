d8f06cb22be171a6bab25832e2a89e5ff376428b
PARK <pwb910130@gmail.com>
2017-07-19 23:25:11 +0900

    STL sort

--- main.cpp
+++ main.cpp
@@ -15,59 +15,22 @@
 using namespace std;
 
 int a[100000];
-int b[100000];
 
-void sort(int start, int end);
-void merge(int start, int end);
 int main(){
+    
     int n;
     
     scanf("%d", &n);
     
     for(int i=0; i<n; i++){
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
     
-    sort(0,n-1);
+    sort(a,a+n);
     
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++){
         printf("%d\n",a[i]);
+    }
     
     return 0;
-}
-
-void sort(int start, int end){
-    if(start == end){
-        return;
-    }
-    
-    int mid = (start+end)/2;
-    
-    sort(start,mid);
-    sort(mid+1,end);
-    merge(start,end);
-}
-
-void merge(int start, int end){
-    int mid = (start+end)/2;
-    int i = start;
-    int j = mid+1;
-    int k=0;
-    
-    while(i<=mid&&j<=end){
-        if(a[i]<=a[j]){
-            b[k++] =a[i++];
-        }else{
-            b[k++]=a[j++];
-        }
-    }
-    while(i<=mid){
-        b[k++] = a[i++];
-    }
-    while(j<=end){
-        b[k++]=a[j++];
-    }
-    for(int i = start; i<=end; i++){
-        a[i] = b[i-start];
-    }
 }
