cce861222c801fe887d96e65d51db540caf1c2e0
PARK <pwb910130@gmail.com>
2017-07-20 15:22:19 +0900

    STL sort cmp function

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ using namespace std;
 
 int a[100000];
 
+bool cmp(const int &a, const int&b){
+    if(a>b)// <-내림 차순 (a<b) 오름차순
+        return true;
+    else
+        return false;
+}
+
 int main(){
     
     int n;
@@ -26,7 +33,7 @@ int main(){
         scanf("%d", &a[i]);
     }
     
-    sort(a,a+n);
+    sort(a,a+n,cmp);
     
     for(int i=0; i<n; i++){
         printf("%d\n",a[i]);
